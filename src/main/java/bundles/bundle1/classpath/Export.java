a90f97f2f01c7974968fb3ea30e2e322ab19e1a3
高云帆 <598335769@qq.com>
2019-03-05 10:24:51 +0800

    init encry class

--- src/main/java/bundles/bundle1/classpath/Export.java
+++
@@ -1,7 +0,0 @@
-package bundles.bundle1.classpath;
-
-public class Export{
-    public void say() {
-        System.out.println("Hello I'm Export,I'm loaded by "+Export.class.getClassLoader());
-    }
-}
